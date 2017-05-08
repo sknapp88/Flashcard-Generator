88a23ee543e11a34117770432467f801c8cc2973
sknapp88 <knapp533@gmail.com>
2017-05-08 16:14:43 -0400

    added partial functionality to flashcards.js and added flashLog for data persistency

--- flashCards.js
+++ flashCards.js
@@ -1,29 +1,52 @@
 var ClozeCard = require("./clozeCard");
 var BasicCard = require("./basicCard");
 var inquirer = require("inquirer");
+var fs = require('fs');
 
 module.exports = BasicCard;
-
-var washington = new ClozeCard("George Washinton was the first president of the united states.", "was the first president of the united states.", "George Washinton");
-console.log(washington.partial);
-
-
-module.exports = ClozeCard();
 
 var create = function(arg1, arg2){
 	switch(arg1){
 		case "cloze":
-		createCloze(arg2);
+		createCloze();
 		break;
 
 		case "basic":
-		createBasic(arg2);
+		createBasic();
 		break;
 	}
 };
 
 function createCloze(){
-
+	inquirer.prompt([
+	{
+		type: "input",
+		message: "what is the complete text?",
+		name: "fullText"
+	},
+	{
+		type: "input",
+		message: "what is the cloze text?",
+		name: "cloze"
+	},
+	{
+		type: "input",
+		message: "what is the partial text?",
+		name: "partial"
+	}
+	]).then(function(answers){
+		fs.appendFile('flashLog.txt', JSON.stringify(answers), (err) => {
+			if (err) throw err;
+			console.log(answers.partial);
+		});
+	});
 }
 
+function clozeQuiz(){fs.readFile('flashLog.txt', (err, data) => {
+	if (err) throw err;
+	console.log(data.partial);
+});
+}
+
+clozeQuiz();
 create(process.argv[2], process.argv[3]);
